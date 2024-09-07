2dd694cf755dc724ad6c104f1ba134d9d6f1f3e2
tyler-ham05 <tyjham05@gmail.com>
2024-09-07 01:35:06 -0700

    Update interface.py
    started working on to do list app portion

--- interface.py
+++ interface.py
@@ -10,24 +10,30 @@ window.geometry('300x150') #('HeightxWidth')
 
 # title
 title_label = ttk.Label(master = window, text = 'Daily Tasks',font = 'Calibri 24 bold') #('Font Size Extra')
-title_label.pack()
+title_label.grid(row = 0, column = 0, pady = 2)
+
+# initial tasks
+def packing():
+    for i in range(0,len(tasks)):
+        checkbox = ttk.Checkbutton(master = window, text = tasks[i])
+        checkbox.grid(row = 4 + i, column = 0, pady = 2)
+packing()
+
+# add task
+def addTask():
+    if len(entry_str.get()) > 0:
+        tasks.append(entry_str.get())
+        packing()
 
 # input field
-"""for i in range(0,len(tasks)):
-    checkbox = ttk.Checkbutton(master = window, text = tasks[0]).grid(row=i)
-    checkbox.pack()"""
 input_frame = ttk.Frame(master = window)
-entry_int = tk.IntVar()
-entry = ttk.Entry(master = input_frame, textvariable = entry_int)
-button = ttk.Button(master = input_frame, text = 'Add Task')
-entry.pack(side = 'bottom') 
-button.pack(side = 'bottom')
-input_frame.pack(pady = 10)
+entry_str = tk.StringVar()
+entry = ttk.Entry(master = input_frame, textvariable = entry_str)
+button = ttk.Button(master = input_frame, text = 'Add Task', command = addTask )
+entry.grid(row = 1, column = 0, pady = 2) 
+button.grid(row = 2, column = 0, pady = 2)
+input_frame.grid(row = 3, column = 0, pady = 2)
 
-# output
-output_string = tk.StringVar()
-output_label = ttk.Label(master = window, text = 'Output', font = 'Calibri 24', textvariable = output_string)
-output_label.pack()
 
 # run
 window.mainloop()
